cc2a5bfac6e126825c702b479a468cbacf0ee54a
Shmuel-Smadar <34glass43@gmail.com>
2024-12-14 12:37:42 +0200

    fix constants

--- server/constants.py
+++ server/constants.py
@@ -1,12 +1,14 @@
 class ClientCodes:
     REGISTRATION = 600
+    CLIENT_LIST = 601
 
 class ServerCodes:
     VERSION = 1
     REGISTRATION_SUCCESSFUL = 2100
+    RETURNED_CLIENT_LIST = 2101
     ERROR = 9000
 
 class ProtocolLengths:
     NAME = 255
     PUBLIC_KEY = 160
-    HEADER = 7
+    HEADER = 23
