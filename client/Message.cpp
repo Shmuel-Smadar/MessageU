c673e3082e2a59cc77b9eae758340e43c1bc3592
Shmuel-Smadar <34glass43@gmail.com>
2024-12-22 17:09:23 +0200

    change message structure and add prrinting method

--- client/Message.cpp
+++ client/Message.cpp
@@ -1,16 +1,19 @@
 #include "Message.h"
 
 
-Message::Message(const std::string& senderClientId, const uint8_t& messageType, const std::string& content) {
-	this->senderClientId = senderClientId;
+Message::Message(UserInfo* userInfo, const uint8_t& messageType, const std::string& content) {
+	this->userInfo = userInfo;
 	this->messageType = messageType;
 	this->content = content;
 }
 
 std::string Message::getSenderClientId() const {
-	return senderClientId;
+	return this->userInfo->getClientID();
 }
 
+std::string Message::getSenderUserName() const {
+	return this->userInfo->getName();
+}
 uint8_t Message::getMessageType() const {
 	return messageType;
 }
@@ -18,7 +21,26 @@ uint8_t Message::getMessageType() const {
 std::string Message::getContent() const {
 	return content;
 }
+UserInfo* Message::getUser() const {
+	return this->userInfo;
+}
+void Message::setContent(const std::string& content) {
+	this->content = content;
+}
+
 void Message::printMessage() const {
+	std::cout << "From: <" << this->getSenderUserName() << ">" << std::endl;
+	std::cout << "Content:" << std::endl;
+	if (this->getMessageType() == MessageType::SymmetricKeyRequest) {
+		std::cout << "Request for symmetric key" << std::endl;
+	}
+	else if (this->getMessageType() == MessageType::SymmetricKeySent) {
+		std::cout << "Symmetric key received" << std::endl;
+	}
+	else if (this->getMessageType() == MessageType::TextMessageSent) {
+		std::cout << this->getContent() << std::endl;
+	}
+	std::cout << "-----<EOM>-----" << std::endl;
 
 }
 
--- client/Message.h
+++ client/Message.h
@@ -2,21 +2,27 @@
 
 #include <string>
 #include <cstdint>
+#include <iostream>
+#include "ProtocolConstants.h"
+#include "UserInfo.h"
 
 class Message {
 private:
-    std::string senderClientId;
+    UserInfo* userInfo;
     uint8_t messageType;
     std::string content;
     //TODO: add checks that the content size isnt bigger than 32 bytes.
 
 public:
-    Message(const std::string& senderClientId, const uint8_t& messageType,const std::string& content);
+    Message(UserInfo* userInfo, const uint8_t& messageType,const std::string& content);
 
     std::string getSenderClientId() const;
+    std::string getSenderUserName() const;
     uint8_t getMessageType() const;
     std::string getContent() const;
+    UserInfo* getUser() const;
+    void setContent(const std::string& content);
     void printMessage() const;
-
+    
 
 };
